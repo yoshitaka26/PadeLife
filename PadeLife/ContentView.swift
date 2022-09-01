263aae3e22e78c52d6931c0e9724746b368a0b4b
yoshitaka <yoshitaka.tanaka.1989@gmail.com>
2022-09-01 23:30:19 +0900

    create app

---
+++ PadeLife/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PadeLife
+//
+//  Created by Yoshitaka Tanaka on 2022/09/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PadeLife/PadeLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadeLifeApp.swift
+//  PadeLife
+//
+//  Created by Yoshitaka Tanaka on 2022/09/01.
+//
+
+import SwiftUI
+
+@main
+struct PadeLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
