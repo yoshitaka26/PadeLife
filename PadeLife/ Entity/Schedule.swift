56205a04b5f68df46c1cf650d1f916ea201c69ca
yoshitaka <yoshitaka.tanaka.1989@gmail.com>
2022-09-24 13:59:40 +0900

    ✨add navigationLink on ScheduleView

--- PadeLife/ Entity/Schedule.swift
+++ PadeLife/ Entity/Schedule.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct PLSchedule {
+struct PLSchedule: Identifiable {
     let id: UUID
     let place: String
     let date: String
--- PadeLife/Common/ScheduleView.swift
+++ PadeLife/Common/ScheduleView.swift
@@ -9,54 +9,100 @@ import SwiftUI
 
 struct ScheduleView: View {
     var plSchedule: PLSchedule
+    
+    enum ScheduleViewDestination {
+        case detail
+        case follow
+        case going
+    }
+
     var body: some View {
-        HStack {
-            Grid(alignment: .leading, verticalSpacing: 5) {
-                GridRow {
-                    Image(systemName: "calendar.badge.clock")
-                        .frame(width: 40, alignment: .center)
-                    Text("日時")
-                    Text(plSchedule.date.description)
+        ZStack {
+            HStack(alignment: .top) {
+                Grid(alignment: .leading, verticalSpacing: 5) {
+                    GridRow {
+                        Image(systemName: "calendar.badge.clock")
+                            .frame(width: 40, alignment: .center)
+                        Text("日時")
+                        Text(plSchedule.date.description)
+                    }
+                    GridRow {
+                        Image(systemName: "mappin.and.ellipse")
+                            .frame(width: 40, alignment: .center)
+                        Text("場所")
+                        Text(plSchedule.place)
+                    }
+                    GridRow {
+                        Image(systemName: "chart.bar")
+                            .frame(width: 40, alignment: .center)
+                        Text("レベル")
+                        Text(plSchedule.levelValue)
+                    }
+                    GridRow {
+                        Image(systemName: "circle.dashed")
+                            .frame(width: 40, alignment: .center)
+                        Text("状態")
+                        Text(plSchedule.statusValue)
+                    }
+                    GridRow {
+                        Image(systemName: "person.3")
+                            .frame(width: 40, alignment: .center)
+                        Text("人数")
+                        Text(plSchedule.participantNumber.description) +
+                        Text("／") +
+                        Text(plSchedule.maximumNumber.description)
+                    }
                 }
-                GridRow {
-                    Image(systemName: "mappin.and.ellipse")
-                        .frame(width: 40, alignment: .center)
-                    Text("場所")
-                    Text(plSchedule.place)
-                }
-                GridRow {
-                    Image(systemName: "chart.bar")
-                        .frame(width: 40, alignment: .center)
-                    Text("レベル")
-                    Text(plSchedule.levelValue)
-                }
-                GridRow {
-                    Image(systemName: "circle.dashed")
-                        .frame(width: 40, alignment: .center)
-                    Text("状態")
-                    Text(plSchedule.statusValue)
-                }
-                GridRow {
-                    Image(systemName: "person.3")
-                        .frame(width: 40, alignment: .center)
-                    Text("人数")
-                    Text(plSchedule.participantNumber.description) +
-                    Text("／") +
-                    Text(plSchedule.maximumNumber.description)
+                .font(.callout)
+                Spacer()
+                HStack(alignment: .top) {
+                    Menu {
+                        NavigationLink(value: ScheduleViewDestination.follow) {
+                            Label {
+                                Text("フォロー")
+                            } icon: {
+                                Image(systemName: "paperclip")
+                            }
+                        }
+                        NavigationLink(value: ScheduleViewDestination.going) {
+                            Label {
+                                Text("参加する")
+                            } icon: {
+                                Image(systemName: "checkmark")
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "ellipsis")
+                            .padding(5)
+                    }
                 }
             }
-            .font(.callout)
-            Spacer()
+            NavigationLink(value: ScheduleViewDestination.detail) {
+                EmptyView()
+            }
+            .opacity(0)
         }
         .padding(10)
         .overlay(RoundedRectangle(cornerRadius: 20)
             .stroke(Color.gray, lineWidth: 3))
         .clipShape(RoundedRectangle(cornerRadius: 20))
+        .navigationDestination(for: ScheduleViewDestination.self) { destination in
+            switch destination {
+            case .detail:
+                Text("詳細画面開発中")
+            case .follow:
+                Text("フォロー開発中")
+            case .going:
+                Text("参加画面開発中")
+            }
+        }
     }
 }
 
 struct ScheduleView_Previews: PreviewProvider {
     static var previews: some View {
-        ScheduleView(plSchedule: mockPLSchedule)
+        NavigationStack {
+            ScheduleView(plSchedule: mockPLSchedule)
+        }
     }
 }
