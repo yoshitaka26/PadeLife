d6d7e1e56e6159d1d72b097e9d1e2d6af176181f
yoshitaka <yoshitaka.tanaka.1989@gmail.com>
2022-10-16 22:58:06 +0900

    📚add staging file

--- PadeLife/PadeLifeApp.swift
+++ PadeLife/PadeLifeApp.swift
@@ -9,11 +9,26 @@ import SwiftUI
 import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+#if RELEASE
+        let resource = "GoogleService-Info"
+#elseif STAGING
+        let resource = "GoogleService-Info-stg"
+#else
+        let resource = "GoogleService-Info-stg"
+#endif
+        
+        if let filePath = Bundle.main.path(forResource: resource, ofType: "plist") {
+            guard let options = FirebaseOptions(contentsOfFile: filePath) else {
+                assert(false, "Could not load config file.")
+            }
+            FirebaseApp.configure(options: options)
+        } else {
+            FirebaseApp.configure()
+        }
+        return true
+    }
 }
 
 @main
